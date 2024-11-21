81bd1e4b44ae9b290d2294bc4bb498b1a2839c9c
yasuhito1105 <yatti10@gmail.com>
2024-11-21 17:18:49 +0900

    deviseの導入　モデルの作成（user prototype comment)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :comments
+  devise_for :prototypes
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
