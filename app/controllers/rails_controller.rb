0b7185ad1b89605e135fd5f101d688aefc7fb4b6
r-ichinose <reoyinolai719@gmail.com>
2024-11-21 15:07:11 +0900

    README実装とフロント実装準備

---
+++ app/controllers/rails_controller.rb
@@ -0,0 +1,13 @@
+class RailsController < ApplicationController
+  def g
+  end
+
+  def controller
+  end
+
+  def prototypes
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'rails/g'
+  get 'rails/controller'
+  get 'rails/prototypes'
+  get 'rails/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "prototypes#index"
 end
