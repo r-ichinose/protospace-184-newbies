b3c7badbfd5a3d406925d83319b64a87683b2f12
yasuhito1105 <yatti10@gmail.com>
2024-11-22 18:01:01 +0900

    モデル削除とトップページ

--- app/models/comment.rb
+++
@@ -1,8 +0,0 @@
-class Comment < ApplicationRecord
-  validates :text, presence: true
-  validates :references, presence: true
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-  validates :text, presence: true
-  validates :string, presence: true
+  has_many :prototypes
+  has_many :comments
+
+  validates :name, presence: true
+  validates :profile, presence: true
   validates :references, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
