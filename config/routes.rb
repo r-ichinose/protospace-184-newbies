e57d054e7d9000646d8a2ec56988f2aa7d1f4fe4
yasuhito1105 <yatti10@gmail.com>
2024-11-23 14:58:49 +0900

    routesの記述が違う？と思ったのと、メアドから名前を記入しないと登録できないという変更です

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
- 
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "prototypes#index"
+  root to: 'prototypes#index'
+  resources :users,only: [:new, :create]
   resources :prototypes
-  resources :users
-end
+  end
