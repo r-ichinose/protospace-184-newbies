65d140cd52a71d93b147a8cb5291760c01e3785e
r-ichinose <reoyinolai719@gmail.com>
2024-11-24 17:14:17 +0900

    ログイン制限の変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,13 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :authenticate_user!, except: [:index]
   def index
   end
 
   private
-
-  def move_to_index
-    return if user_signed_in?
-
-    redirect_to action: :index
-  end
 end
