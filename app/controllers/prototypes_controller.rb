8cd2fcc7c9a222183e1b2d4879d8577a6e00f7d1
mxmldesign <nesignsprivate@gmail.com>
2024-11-26 12:20:04 +0900

    編集機能を実装
    editアクションにインスタンス変数@prototypeを定義した。且つ、Pathパラメータで送信されるID値で、Prototypeモデルの特定のオブジェクトを取得するように記述し、それを@prototypeに代入した
     edit.html.erbから部分テンプレートである、_form.html.erbを呼び出す記述をした
     updateアクションにデータを更新する記述をし、更新されたときはそのプロトタイプの詳細ページに戻るような記述をした
     updateアクションに、データが更新されなかったときは、編集ページに戻るようにrenderを用いて記述した
     バリデーションによって更新ができず編集ページへ戻ってきた場合でも、入力済みの項目（画像以外）は消えないことを確認した
     サーバーを再起動し、ブラウザで正しく動くか確認した
     Sequel Proを確認して、正しく更新ができているか確認した

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,9 +22,16 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
   end
 
   def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
