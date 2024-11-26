3ee5249d373ae233555b5e1a631790ea746fb163
yasuhito1105 <yatti10@gmail.com>
2024-11-26 18:49:17 +0900

    最終調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @name = current_user.name
-    @profile = current_user.profile
-    @position = current_user.position
-    @prototypes = current_user.prototypes
-    @occupation = current_user.occupation
+    @prototypes = @user.prototypes
   end
 end
